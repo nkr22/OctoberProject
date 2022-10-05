21f3585012515b36a756e4e5c7f9335a1a4d6ccf
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-10-04 22:49:33 -0600

    add donut model

--- octoberwebsite/basic/models.py
+++ octoberwebsite/basic/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Donuts(models.Model):
+    donut_name = models.CharField(max_length=45)
+    donut_url = models.CharField(max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'donuts'
